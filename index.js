e1e71747784968fbb50ff586fa12b76a50ada6b1
jardeveloper <jorgearios@protonmail.com>
2020-10-30 15:48:19 -0300

    Subida

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
+const cors = require('cors');
+// import routes
+const dashboadRoutes = require('./routes/dashboard');
+const verifyToken = require('./routes/validate-token');
+
+require('dotenv').config()
+
+const app = express();
+
+var corsOptions = {
+    origin: '*', // Reemplazar con dominio
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+// capturar body
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+
+// Conexión a Base de datos
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@jorge-m001.jjdov.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`;
+mongoose.connect(uri,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+)
+.then(() => console.log('Base de datos conectada'))
+.catch(e => console.log('error db:', e))
+
+// import routes
+const authRoutes = require('./routes/auth');
+
+
+// route middlewares
+app.use('/api/dashboard', verifyToken, dashboadRoutes);
+app.use('/api/user', authRoutes);
+app.get('/', (req, res) => {
+    res.json({
+        estado: true,
+        mensaje: 'funciona!'
+    })
+});
+
+// iniciar server
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`servidor andando en: ${PORT}`)
+})
