7439aa7650a0786f1a201528de36b9519de2f2af
jardeveloper <jorgearios@protonmail.com>
2020-10-30 15:55:42 -0300

    cambios

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ var corsOptions = {
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
+app.use(cors(corsOptions));
+
 // capturar body
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
